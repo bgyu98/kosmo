50fd450b279b7fe8c475503bd5c4301d983e9bd5
변정환 <bgyu98@gmail.com>
2022-09-14 18:30:39 +0900

    09.14

--- src/chapter07/EX05/ThisMethod04.java
+++ src/chapter07/EX05/ThisMethod04.java
@@ -3,12 +3,20 @@ package chapter07.EX05;
 import chapter05.EX03.ArgumentOfMainMethod;
 
 class Car{
-
+	
+	//this 키워드 : 필드나 메서드 선언시 사용 불가.
+//				-- 생성자, 메서드 내부에서 필드나 메서드를 사용할 때 자신의 객체 필드나 메서드를 지칭
+	// 			-- 자신의 객체의 필드, 메서드를 가르킨다. 기본적으로 생략해서 많이 사용.
+	// 	this()	-- 생성자 내부에서만 사용, 반드시 첫 라인에 위치
+	//			-- 자신의 객체의 다른 생성자를 호출
+	//			-- 생성자를 오버라이딩 할 때 코드를 간략하게 사용
+	
 	String companyName;		//현대
 	String color;			//검은
 	double maxSpeed;		//200.0 Km/h
 	
-	Car(){
+	//생성자
+	Car(){		//리턴 타입이 없다, 클래스 이름과 동일
 		companyName = "현대자동차";
 		color = "검은색";
 		maxSpeed = 200;
